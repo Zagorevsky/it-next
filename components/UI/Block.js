55cd9e9fd8c8623fce5c3b67de7b7676e022d20a
Oleg Zagorevsky <pboul-ooo@yandex.ru>
2022-06-12 17:43:29 +0300

    fix: css

--- components/UI/Block.js
+++ components/UI/Block.js
@@ -3,9 +3,9 @@ import styles from '../blocks/Block.module.css';
 const Block = (props) => {
   return (
     <section className={ styles.block }>
-      <img ></img>
-      <h3>{ props.title }</h3>
-      <p>{ props.txt }</p>
+      <img className={ styles.image }></img>
+      <h3 className={ styles.title }>{ props.title }</h3>
+      <p className={ styles.text }>{ props.txt }</p>
     </section>
   );
 };
